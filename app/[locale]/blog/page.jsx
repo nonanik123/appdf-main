22e2d404accd13d04d84cad690e63e97be6f519a
Hasan Şişik <sisik.hsn@gmail.com>
2024-10-11 18:54:19 +0300

    Refactor blog page to use async/await for fetching markdown data

--- app/[locale]/blog/page.jsx
+++ app/[locale]/blog/page.jsx
@@ -1,17 +1,17 @@
-import FeatureBlog from '@/components/blogs/FeatureBlog'
-import RecentNews from '@/components/blogs/RecentNews'
-import Footer from '@/components/footer/Footer'
-import SecondaryNavbar from '@/components/navbar/SecondaryNavbar'
-import NewsLetter from '@/components/shared/NewsLetter'
-import PageHero from '@/components/shared/PageHero'
-import getMarkDownData from '@/utils/getMarkDownData'
+import FeatureBlog from '@/components/blogs/FeatureBlog';
+import RecentNews from '@/components/blogs/RecentNews';
+import Footer from '@/components/footer/Footer';
+import SecondaryNavbar from '@/components/navbar/SecondaryNavbar';
+import NewsLetter from '@/components/shared/NewsLetter';
+import PageHero from '@/components/shared/PageHero';
+import getMarkDownData from '@/utils/getMarkDownData';
 
 export const metadata = {
   title: 'Blog',
-}
+};
 
-const Blog = () => {
-  const blogs = getMarkDownData('data/blogs')
+const Blog = async () => {
+  const blogs = await  getMarkDownData('data/blogs');
   return (
     <>
       <SecondaryNavbar />
@@ -23,7 +23,7 @@ const Blog = () => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
--- utils/getMarkDownData.js
+++ utils/getMarkDownData.js
@@ -1,25 +1,31 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
 const getMarkDownData = (folder) => {
-  const files = fs.readdirSync(folder)
-  const markdownPosts = files.filter((file) => file.endsWith('.md'))
+  try {
+    const folderPath = path.join(process.cwd(), folder); // Mutlak yol oluşturma
+    const files = fs.readdirSync(folderPath);
+    const markdownPosts = files.filter((file) => file.endsWith('.md'));
 
-  const postsData = markdownPosts.map((file) => {
-    const filePath = path.join(folder, file)
-    const fullContent = fs.readFileSync(filePath, 'utf8')
-    const { data, content } = matter(fullContent)
-    const slug = file.replace('.md', '')
+    const postsData = markdownPosts.map((file) => {
+      const filePath = path.join(folderPath, file);
+      const fullContent = fs.readFileSync(filePath, 'utf8');
+      const { data, content } = matter(fullContent);
+      const slug = file.replace('.md', '');
 
-    return {
-      slug,
-      data,
-      content,
-    }
-  })
+      return {
+        slug,
+        data,
+        content,
+      };
+    });
 
-  return postsData
-}
+    return postsData;
+  } catch (error) {
+    console.error(`Error reading markdown data from folder ${folder}:`, error);
+    return [];
+  }
+};
 
-export default getMarkDownData
+export default getMarkDownData;
