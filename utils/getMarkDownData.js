af2d2c6549f88a772973675bf9f9a55b1c854037
Hasan Şişik <sisik.hsn@gmail.com>
2024-10-11 18:59:38 +0300

    abc

--- utils/getMarkDownData.js
+++ utils/getMarkDownData.js
@@ -1,31 +1,25 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
 const getMarkDownData = (folder) => {
-  try {
-    const folderPath = path.join(process.cwd(), folder); // Mutlak yol oluşturma
-    const files = fs.readdirSync(folderPath);
-    const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  const files = fs.readdirSync(folder)
+  const markdownPosts = files.filter((file) => file.endsWith('.md'))
 
-    const postsData = markdownPosts.map((file) => {
-      const filePath = path.join(folderPath, file);
-      const fullContent = fs.readFileSync(filePath, 'utf8');
-      const { data, content } = matter(fullContent);
-      const slug = file.replace('.md', '');
+  const postsData = markdownPosts.map((file) => {
+    const filePath = path.join(folder, file)
+    const fullContent = fs.readFileSync(filePath, 'utf8')
+    const { data, content } = matter(fullContent)
+    const slug = file.replace('.md', '')
 
-      return {
-        slug,
-        data,
-        content,
-      };
-    });
+    return {
+      slug,
+      data,
+      content,
+    }
+  })
 
-    return postsData;
-  } catch (error) {
-    console.error(`Error reading markdown data from folder ${folder}:`, error);
-    return [];
-  }
-};
+  return postsData
+}
 
-export default getMarkDownData;
+export default getMarkDownData
